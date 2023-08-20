5caac18a1c042b32243149ed3f9b95316b80d2ab
Manelito <emanuelwork1@gmail.com>
2023-08-20 18:10:44 -0300

    feat: adding database persistence on methods by mongoose model

--- src/modules/players/usecases/list-players.ts
+++ src/modules/players/usecases/list-players.ts
@@ -2,10 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { UseCase } from '@/base';
 import { ListPlayersDTO } from '../dtos';
 import { PlayerEntity } from '../../../core/entities/players.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ListUsers implements UseCase<PlayerEntity> {
-  execute(params: ListPlayersDTO): PlayerEntity[] | any[] {
-    return [params];
+  constructor(
+    @InjectModel('Player') private readonly playerModel: Model<PlayerEntity>,
+  ) {}
+  async execute(params: ListPlayersDTO): Promise<PlayerEntity[]> {
+    const { limit, page } = params;
+
+    let query = this.playerModel.find();
+
+    if (limit && page) {
+      const skipCount = Number(limit) * page;
+      query = query.limit(Number(limit)).skip(skipCount);
+    }
+
+    const players = await query.exec();
+    return players;
   }
 }
