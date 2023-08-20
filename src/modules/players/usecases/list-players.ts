f64abc76ab89a80338ad6c7005f24dde8e1f20a1
Manelito <emanuelwork1@gmail.com>
2023-08-20 15:02:04 -0300

    feat: change entity layer

--- src/modules/players/usecases/list-players.ts
+++ src/modules/players/usecases/list-players.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UseCase } from '@/base';
 import { ListPlayersDTO } from '../dtos';
-import { PlayerEntity } from '../../entities/players.entity';
+import { PlayerEntity } from '../../../entities/players.entity';
 
 @Injectable()
 export class ListUsers implements UseCase<PlayerEntity> {
