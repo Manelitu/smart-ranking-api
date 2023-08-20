4104bb1c04cdc14c07b221bc87abbbfa30a24afd
Manelito <emanuelwork1@gmail.com>
2023-08-20 16:12:56 -0300

    fix: adjust _id to add on mongodb

--- src/core/entities/players.entity.ts
+++ src/core/entities/players.entity.ts
@@ -1,17 +1,11 @@
-import { UUID, randomUUID } from 'crypto';
 import { Entity } from '@/base';
 
 export class PlayerEntity extends Entity {
-  public _id: UUID;
+  public _id: string;
   public phone: string;
   public email: string;
   public name: string;
   public ranking: string;
   public rankingPosition: number;
   public image: string;
-
-  constructor() {
-    super();
-    this._id = randomUUID();
-  }
 }
