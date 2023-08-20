8dd67490abfefb776f1d2a79fbf940e83c0e211e
Manelito <emanuelwork1@gmail.com>
2023-08-20 14:14:42 -0300

    feat: create player mapper

---
+++ src/helpers/mappers/players.mapper.ts
@@ -0,0 +1,22 @@
+import { Mapper } from '../../base/mapper';
+import { CreatePlayerDTO } from '../../modules/players/dtos/create-player.dto';
+import { PlayerEntity } from '../../modules/players/entities/players.entity';
+import { copyProperties } from '../copyProperties';
+
+export class CreatePlayerMapper extends Mapper<CreatePlayerDTO, PlayerEntity> {
+  mapFrom(param: CreatePlayerDTO): PlayerEntity {
+    const playerEntity = new PlayerEntity();
+    copyProperties(param, playerEntity);
+    console.log(playerEntity);
+    return playerEntity;
+  }
+
+  mapTo(param: PlayerEntity): CreatePlayerDTO {
+    const { phone, email, name } = param;
+    return {
+      phone,
+      email,
+      name,
+    };
+  }
+}
