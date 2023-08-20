799e6292f78a7c803a31649c858e25faaa260a80
Manelito <emanuelwork1@gmail.com>
2023-08-20 14:15:32 -0300

    feat: importing in providers

--- src/modules/players/players.module.ts
+++ src/modules/players/players.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
+import { CreatePlayer } from './usecases/create-player';
 
 @Module({
   controllers: [PlayersController],
+  providers: [CreatePlayer],
 })
 export class PlayersModule {}
