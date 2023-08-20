1b3df150411a7417378df0fb5daff985b8476ff9
Manelito <emanuelwork1@gmail.com>
2023-08-20 14:56:52 -0300

    feat: adding ListUsers on module

--- src/modules/players/players.module.ts
+++ src/modules/players/players.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PlayersController } from './players.controller';
 import { CreatePlayer } from './usecases/create-player';
+import { ListUsers } from './usecases/list-users';
 
 @Module({
   controllers: [PlayersController],
-  providers: [CreatePlayer],
+  providers: [CreatePlayer, ListUsers],
 })
 export class PlayersModule {}
