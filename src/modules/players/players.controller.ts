9430985e728fb1bcafd7a4717cfd41292be2fa98
Manelito <emanuelwork1@gmail.com>
2023-08-20 14:15:41 -0300

    feat: create player controller

--- src/modules/players/players.controller.ts
+++ src/modules/players/players.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlayerDTO } from './dtos/create-player.dto';
+import { CreatePlayer } from './usecases/create-player';
 
 @Controller('api/v1/players')
 export class PlayersController {
+  constructor(private createPlayer: CreatePlayer) {}
+
   @Post()
-  async saveAndUpdate() {
-    return JSON.stringify('Teste');
+  async saveAndUpdate(@Body() createPlayerDTO: CreatePlayerDTO) {
+    const response = this.createPlayer.execute(createPlayerDTO);
+    return JSON.stringify(response);
   }
 }
