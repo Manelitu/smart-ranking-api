31cd630b4511306bfbd9d07e6aea787907a00166
Manelito <emanuelwork1@gmail.com>
2023-08-20 14:56:06 -0300

    feat: create list method on controller

--- src/modules/players/players.controller.ts
+++ src/modules/players/players.controller.ts
@@ -1,14 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreatePlayerDTO } from './dtos/create-player.dto';
 import { CreatePlayer } from './usecases/create-player';
+import { ListUsersDTO } from './dtos/list-users.dto';
+import { ListUsers } from './usecases/list-users';
 
 @Controller('api/v1/players')
 export class PlayersController {
-  constructor(private createPlayer: CreatePlayer) {}
+  constructor(
+    private createPlayer: CreatePlayer,
+    private listUsers: ListUsers,
+  ) {}
 
   @Post()
   async saveAndUpdate(@Body() createPlayerDTO: CreatePlayerDTO) {
     const response = this.createPlayer.execute(createPlayerDTO);
     return JSON.stringify(response);
+  }
+
+  async list(@Body() listUsersDTO: ListUsersDTO) {
+    return this.listUsers.execute(listUsersDTO);
   }
 }
