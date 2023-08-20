d790e24fb188b3d823b1fa92ebb0e87b2e208238
Manelito <emanuelwork1@gmail.com>
2023-08-20 16:12:15 -0300

    feat: adding config mongooseModule for feature

--- src/modules/players/players.module.ts
+++ src/modules/players/players.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersController } from './players.controller';
 import { CreatePlayer } from './usecases/create-player';
 import { ListUsers } from './usecases/list-players';
+import { playerSchema } from './repositories/mongoose/schemas/players.schema';
 
+const mongooseConfig = { name: 'Player', schema: playerSchema };
 @Module({
+  imports: [MongooseModule.forFeature([mongooseConfig])],
   controllers: [PlayersController],
   providers: [CreatePlayer, ListUsers],
 })
