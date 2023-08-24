9aecdf172cf8331fb5d140a3fc1c1d4d7380ec2e
Manelito <emanuelwork1@gmail.com>
2023-08-24 19:14:17 -0300

    feat: create category use case create

--- src/modules/categories/usecases/create-category.ts
+++ src/modules/categories/usecases/create-category.ts
@@ -1,5 +1,4 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePlayerMapper } from '@/modules/players/mappers/create-player.mapper';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { UseCase } from '@/base';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -8,14 +7,18 @@ import { CategoryEntity } from '@/core/entities/category.entity';
 
 @Injectable()
 export class CreateCategory implements UseCase<CategoryEntity> {
-  private mapper: CreatePlayerMapper = new CreatePlayerMapper();
-
   constructor(
     @InjectModel('Category')
     private readonly categoryModel: Model<CategoryEntity>,
   ) {}
 
   async execute(param: CreateCategoryDTO): Promise<CategoryEntity> {
-    return this.categoryModel.create(param);
+    const { category } = param;
+    const hasCategory = await this.categoryModel.findOne({ category }).exec();
+
+    if (hasCategory) throw new ConflictException('Category already exists');
+
+    const newCategory = new this.categoryModel(param);
+    return newCategory.save();
   }
 }
