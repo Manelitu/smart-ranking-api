c9201021d4ac6c88281c4c0a8bd4a3319627edf1
Manelito <emanuelwork1@gmail.com>
2023-08-24 18:22:03 -0300

    feat: create categories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { PlayersModule } from './modules/players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { CategoriesController } from './modules/categories/categories.controller';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL),
     PlayersModule,
+    CategoriesModule,
   ],
-  controllers: [],
+  controllers: [CategoriesController],
   providers: [],
 })
 export class AppModule {}
