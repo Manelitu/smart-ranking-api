f649dd0f13c9408b7604ea99b99dc4f0b0b4d8f3
Manelito <emanuelwork1@gmail.com>
2023-08-24 19:13:37 -0300

    feat: adding mongoose module on category module

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { CreateCategory } from './usecases';
+import { CategoriesController } from './categories.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { categorySchema } from './repositories/mongoose/schemas/categories.schema';
+
+const mongooseConfig = { name: 'Category', schema: categorySchema };
 
 @Module({
+  imports: [MongooseModule.forFeature([mongooseConfig])],
   providers: [CreateCategory],
+  controllers: [CategoriesController],
 })
 export class CategoriesModule {}
