52efad0c8a0bd9871a4576d9b696e44a58702cb4
Manelito <emanuelwork1@gmail.com>
2023-09-03 16:42:14 -0300

    feat: add exceptions config on main

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { configCors, configSwagger, configClassValidator } from './config';
+import {
+  configCors,
+  configSwagger,
+  configClassValidator,
+  configGlobalFilters,
+} from './config';
+import { AllExceptionsFilter } from './core/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   configCors(app);
   configClassValidator(app);
+  configGlobalFilters(app, new AllExceptionsFilter());
   configSwagger({
     app: app,
     config: {
